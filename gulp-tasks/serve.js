0fc821ba37be94ac45f4bff8ad9040f2d0dec667
John Cionci <john@thinkoomph.com>
2018-04-06 13:24:10 -0400

    nrowsersync cleanup

--- gulp-tasks/serve.js
+++ gulp-tasks/serve.js
@@ -8,5 +8,5 @@
 module.exports = function (gulp, plugins, options) {
   'use strict';
 
-  gulp.task('serve', ['build:dev', 'browser-sync', 'watch']);
+  gulp.task('serve', ['build:dev', 'watch']);
 };
